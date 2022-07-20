0df0fa035fb44d95ed8d0a9dd0a86ba8cf15f03d
brujoand <anders@brujordet.no>
2022-07-20 08:57:49 +0200

    Adjusting bicycle chain to measured size

--- bicycle_chain_guard/bicycle_chain_guard.js
+++ bicycle_chain_guard/bicycle_chain_guard.js
@@ -1,13 +1,13 @@
 const jscad = require('@jscad/modeling')
 const { union, subtract } = jscad.booleans
 const { cylinder, cuboid } = jscad.primitives
-const { mirrorX, mirrorY, rotateX, rotateY, rotateZ } = jscad.transforms
+const { rotateZ } = jscad.transforms
 const { degToRad } = jscad.utils
 
 
 function main (){
 
-  const mainCircleRadius = 90
+  const mainCircleRadius = 82.5
   const mainCircleWidth = 20
   const mainCircleThickness = 3
 
@@ -15,9 +15,12 @@ function main (){
   const rimThickness = 9
   const rimRadius = mainCircleRadius - mainCircleWidth + rimWidth
 
+  const toothCount = 5
   const toothRadius = 10
   const toothConnectorLength = 10
   const toothThickness = 9
+  const toothConnectorPositionZ = (mainCircleRadius - mainCircleWidth) - (toothRadius / 2 )
+  const toothPositionZ = toothConnectorPositionZ - (toothConnectorLength / 2)
 
   const nutClearingRadius = 3.75
   const nutRadius = 2
@@ -28,36 +31,32 @@ function main (){
   const rimOuter = cylinder({radius: rimRadius, height: rimThickness, center: [0,0,rimThickness/2], segments: 64})
   const rimInner = cylinder({radius: rimRadius - rimWidth, height: rimThickness, center: [0,0,rimThickness/2], segments: 64})
 
-  const toothCircle = cylinder({radius: toothRadius, height: toothThickness, center: [60,0,toothThickness/2], segments: 24})
-  const toothConnector = cuboid({size: [toothConnectorLength, toothRadius * 2, toothThickness], center: [65,0,toothThickness/2]})
+  const toothCircle = cylinder({radius: toothRadius, height: toothThickness, center: [toothPositionZ,0,toothThickness/2], segments: 24})
+  const toothConnector = cuboid({size: [toothConnectorLength, toothRadius * 2, toothThickness], center: [toothConnectorPositionZ,0,toothThickness/2]})
 
-  const toothNutClearingHole = cylinder({radius: nutClearingRadius, height: toothThickness/2, center: [60,0,toothThickness/4], segments: 12})
-  const toothNutHole = cylinder({radius: nutRadius, height: toothThickness-2, center: [60,0,toothThickness - (toothThickness/4)], segments: 64})
+  const toothNutClearingHole = cylinder({radius: nutClearingRadius, height: toothThickness/2, center: [toothPositionZ,0,toothThickness/4], segments: 12})
+  const toothNutHole = cylinder({radius: nutRadius, height: toothThickness-2, center: [toothPositionZ,0,toothThickness - (toothThickness/4)], segments: 64})
 
   const tooth = union(toothCircle, toothConnector)
   const nut = union(toothNutClearingHole, toothNutHole)
 
   const toothNut = subtract(tooth, nut)
 
-
   const mainCircle = subtract(mainCircleOuter, mainCircleInner)
   const rim = subtract(rimOuter, rimInner)
 
-  const single_tooth = union(union(mainCircle, rim), toothNut) // men isteden bruker vi denne
+  const single_tooth = union(union(mainCircle, rim), toothNut)
 
-  const angle = degToRad(72)
+  const toothAngle = degToRad(360/toothCount)
 
-  const double_tooth = rotateZ(angle, single_tooth)
-  const tripple_tooth = rotateZ(angle, double_tooth)
-  const quadruple_tooth = rotateZ(angle, tripple_tooth)
-  const pentagon = rotateZ(angle, quadruple_tooth)
+  var full_chain_guard = single_tooth
 
-  const one = union(single_tooth, double_tooth)
-  const two = union(one, tripple_tooth)
-  const three = union(two, quadruple_tooth)
-  const four = union(three, pentagon)
+  for (let i = 0; i < toothCount; i++) {
+    const rotated_chain_guard = rotateZ(toothAngle, full_chain_guard)
+    full_chain_guard = union(full_chain_guard, rotated_chain_guard)
+  }
 
-  return four
+  return full_chain_guard
 
 }
 
