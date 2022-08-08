2de6be23582962572d23f0879f463e64487f9ae8
brujoand <anders@brujordet.no>
2022-08-08 14:41:49 +0200

    Initial radial grill

--- radial_hepa_case/radial_hepa_case.js
+++ radial_hepa_case/radial_hepa_case.js
@@ -16,9 +16,11 @@ const screw_brim_width=20
 function hex_plate(plate) {
   const hex_hole = cylinder({radius: hex_radius - (grill_thickness/2), height: plate_height, segments: 6})
   var hexer = plate
-  for (let i = 0; i < plate_width + hex_radius * 2; i = i + 8 + (hex_radius*2)) {
+  const hex_width = plate_width - screw_brim_width
+  const hex_depth = plate_depth - screw_brim_depth
+  for (let i = 0; i < plate_depth; i = i + 8 + (hex_radius*2)) {
     var line_number=0
-    for (let y = 0; y < plate_depth + hex_radius * 2; y = y + (hex_radius) - 2) {
+    for (let y = 0; y < hex_depth; y = y + (hex_radius) - 2) {
       if (line_number % 2 === 0){
         offset=hex_radius + 4 + (screw_brim_width/2)
       } else {
